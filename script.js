fdfe8e8c07d7b5754668207fb57fc39dfef865f5
alhoff <andrew.hoff14@yahoo.com>
2020-09-19 13:58:05 -0700

    added more button catagories plus a new chuck norris joke api

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let queryURL = "https://sv443.net/jokeapi/v2/joke/Any?type=single";
+$(document).ready(function () {
 
-$(".start-button").on("click", function() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (res) {
-        console.log(res.joke)
-        $(".result").text(res.joke);
-    });
-})
+    $(".programming-button").on("click", function () { // When you click the programming button, fill the textbox with a relevant joke
+        getJoke("Programming");
+    })
+    $(".puns-button").on("click", function () {
+        getJoke("Pun");
+    })
+    $(".dark-button").on("click", function () {
+        getJoke("Dark");
+    })
+    $(".norris-button").on("click", function () { // Grabs from Chuck Norris api instead
+        $.ajax({
+            url: "https://api.chucknorris.io/jokes/random",
+            method: "GET"
+        }).then(function (res) {
+            $(".result").text(res.value);
+        });
+    })
+
+    function getJoke(inputCatagory) {
+        let catagory = inputCatagory;
+        let queryURL = "https://sv443.net/jokeapi/v2/joke/" + catagory + "?type=single";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (res) {
+            $(".result").text(res.joke); // Sets textbox's text to the generated joke
+        });
+    }
+});
