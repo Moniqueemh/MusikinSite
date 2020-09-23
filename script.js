b205e9829fa419c7b01b8586b94b405212556af5
alhoff <andrew.hoff14@yahoo.com>
2020-09-22 20:01:13 -0700

    Added basic input fields for artist and track that logs out the entire response object

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 $(document).ready(function () {
+    let lastFmKey = "10a27986ff454210e0ad746d11440bec"
 
-    $(".programming-button").on("click", function () { // When you click the programming button, fill the textbox with a relevant joke
-        getJoke("Programming");
-    })
-    $(".puns-button").on("click", function () {
-        getJoke("Pun");
-    })
-    $(".dark-button").on("click", function () {
-        getJoke("Dark");
-    })
-    $(".norris-button").on("click", function () { // Grabs from Chuck Norris api instead
+
+    $("#searchBtn").on("click", function () {
+        let lastFmArtist = $("#artistInput").val();
+        let lastFmTrack = $("#trackInput").val();
+        let lastFmTrackInfoURL = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=" + lastFmKey + "&artist=" + lastFmArtist + "&track=" + lastFmTrack + "&format=json"
+
         $.ajax({
-            url: "https://api.chucknorris.io/jokes/random",
+            url: lastFmTrackInfoURL,
             method: "GET"
         }).then(function (res) {
-            $(".result").text(res.value);
+            console.log(res)
         });
     })
 
-    function getJoke(inputCatagory) {
-        let catagory = inputCatagory;
-        let queryURL = "https://sv443.net/jokeapi/v2/joke/" + catagory + "?type=single";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (res) {
-            $(".result").text(res.joke); // Sets textbox's text to the generated joke
-        });
-    }
 });
